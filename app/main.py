1cf38a35a1f56b778b6309768ad82c1665f72074
KazukiKimura-Developer <momx4639@yahoo.co.jp>
2022-08-15 13:32:01 +0900

    コントローラー追加

--- app/main.py
+++ app/main.py
@@ -12,14 +12,12 @@ from sanic.log import logger
 
 from sanic.server.websockets.impl import WebsocketImplProtocol as Websocket
 
-app = Sanic("HackU2022-backend")
 
-@app.get("/")
-async def hello_world(request):
 
-    return response.json({"massage": "Hello World !!"})
+
 
 
 
 if __name__ == '__main__':
+    app = Sanic("HackU2022-backend")
     app.run(host='0.0.0.0', port=8099, auto_reload=True)

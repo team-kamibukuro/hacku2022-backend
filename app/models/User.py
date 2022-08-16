599f159f47e23f7b7b3270b5433c0f05474cd3ef
KazukiKimura-Developer <momx4639@yahoo.co.jp>
2022-08-16 10:26:03 +0900

    Userモデルのコンストラクタを追加

--- app/models/User.py
+++ app/models/User.py
@@ -21,18 +21,18 @@ class User(Base):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
 
-    id = Column('id', String(255), primary_key=True, default=str(uuid.uuid4()))
+    id = Column('id', String(255), primary_key=True)
     usersName = Column('users_name', String(255))
     usersEmail = Column('users_mail', String(255))
     usersPassword = Column('users_password', String(255))
     createdAt = Column('created_at', DateTime, default=datetime.datetime.now(), nullable=False)
 
-    # def __init__(self, usersName, usersEmail, usersPassword):
-    #     self.id = str(uuid.uuid4())
-    #     self.usersName = usersName
-    #     self.usersEmail = usersEmail
-    #     self.usersPassword = usersPassword
-    #     self.createdAt = datetime.datetime.now()
+    def __init__(self, usersName, usersEmail, usersPassword):
+        self.id = str(uuid.uuid4())
+        self.usersName = usersName
+        self.usersEmail = usersEmail
+        self.usersPassword = usersPassword
+        self.createdAt = datetime.datetime.now()
 
 
 
--- app/services/UserService.py
+++ app/services/UserService.py
@@ -5,14 +5,14 @@ from models.User import User
 from repository.UserRepository import *
 from setting import async_session
 
+
 class UserService:
 
-    async def userSiginup(request):
-
-        user = User.User()
-        user.usersEmail = request.json['userEmail']
-        user.usersName = request.json['userName']
-        user.usersPassword = request.json['userPassword']
+    async def userSiginup(self):
+        user = User.User(
+            usersName=self.json['userName'],
+            usersEmail=self.json['userEmail'],
+            usersPassword=self.json['userPassword'])
 
         await UserRepository.save(user)
 
@@ -20,4 +20,4 @@ class UserService:
         #     session.add(user)
         #     await session.commit()
 
-        return response.json("{\"name\":\"ok\"}")
+        return response.json({"name": "ok"})
